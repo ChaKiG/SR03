98f41b536418d38f77fba96e3644f7f289d6a17f
Fabrice De Régibus <fabrice.de-regibus@etu.utc.fr>
2016-03-21 18:06:16 +0100

    Ajout des servlets

---
+++ Projet_trombi/src/trombi/PersonneServlet.java
@@ -0,0 +1,53 @@
+package trombi;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Servlet implementation class PersonneServlet
+ */
+@WebServlet("/PersonneServlet")
+public class PersonneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PersonneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nom, prenom;
+		nom = request.getParameter("nom");
+		prenom = request.getParameter("prenom");
+		
+		String text = "";
+		ObjectMapper map = new ObjectMapper();
+		Personne[] lPersonnes = new DataHandler(nom, prenom).getPersonnes();
+		text =  map.writeValueAsString(lPersonnes);
+			
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(text);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
