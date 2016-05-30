af7ef3363f366d187e02a72cd3066e9a005a37e6
Fabrice De Régibus <fabrice.de-regibus@etu.utc.fr>
2016-05-30 16:52:01 +0200

    Web service calculette ajouté

--- calculette/src/calculette/Calculette.java
+++ calculette/src/calculette/Calculette.java
@@ -2,16 +2,15 @@ package calculette;
 
 public class Calculette {
 	
-	
-	public int add( int val1, int val2) {
+	public static int add( int val1, int val2) {
 		return val1 + val2;
 	}
 	
-	public int mul(int val1, int val2){
+	public static int mul(int val1, int val2){
 		return val1*val2;
 	}
 	
-	public int sou(int val1, int val2){
+	public static int sou(int val1, int val2){
 		return val1-val2;
 	}
 }
