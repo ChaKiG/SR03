15963229985c833167b9b2a903a7bc318801f726
Fabrice De Régibus <fabrice.de-regibus@etu.utc.fr>
2016-05-30 17:19:08 +0200

    Modification main

--- client/src/calculette/Main.java
+++ client/src/calculette/Main.java
@@ -3,11 +3,9 @@ package calculette;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		CalculetteProxy c = new CalculetteProxy( "http://localhost:8080/calculette/services/Calculette");
-		CalculetteProxy c = new CalculetteProxy( );
+		CalculetteProxy c = new CalculetteProxy( "http://localhost:8080/calculette/services/Calculette");
 		try {
-			c.add(3, 4);	
+			System.out.println(c.add(3, 4));	
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
