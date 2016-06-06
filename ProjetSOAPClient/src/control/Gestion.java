0a68fe93c7b35313b28b58820eb3fd5b7bdf56c3
ChaKiG <thms.pelletier@gmail.com>
2016-06-06 18:10:36 +0200

    client update

--- ProjetSOAPClient/src/control/Gestion.java
+++ ProjetSOAPClient/src/control/Gestion.java
@@ -52,14 +52,19 @@ public class Gestion {
 				this.a.setTelephone(Integer.valueOf(val[0]));
 				break;
 			case "adresse.id":
+				a.getAdresse().setId(Integer.valueOf(val[0]));
 				break;
 			case "adresse.CP":
+				a.getAdresse().setCode_postal(Integer.valueOf(val[0]));
 				break;
 			case "adresse.ville":
+				a.getAdresse().setVille(val[0]);
 				break;
 			case "adresse.rue":
+				a.getAdresse().setRue(val[0]);
 				break;
 			case "adresse.num":
+				a.getAdresse().setNumero(Integer.valueOf(val[0]));
 				break;
 			}
 		}
@@ -69,7 +74,7 @@ public class Gestion {
 		annoncesSOAP.AnnonceSOAPProxy p = new annoncesSOAP.AnnonceSOAPProxy("");
 		List<Annonce> annonces = null;
 		try {
-			if (annonceId > 0) {
+			if (a.getId() > 0) {
 			p.getAnnonces();
 			} else if (categorie > 0) {
 				annonces = new ArrayList<Annonce>(Arrays.asList(p.getAnnoncesCat(categorie)));
@@ -87,8 +92,5 @@ public class Gestion {
 	private int updateAnnonce() {return -1;}
 	private int deleteAnnonce() {return -1;}
 	
-	private int updateAnnonce() {return -1;}
-	private int updateAnnonce() {return -1;}
-	private int updateAnnonce() {return -1;}
 	
 }
